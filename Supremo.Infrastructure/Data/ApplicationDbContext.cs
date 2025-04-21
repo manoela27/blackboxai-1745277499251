fadec1403a39365b5c4d765f6d721807235a88ca
Manoela Moreno Frattini <84809830+manoela27@users.noreply.github.com>
2025-04-21 20:18:36 -0300

    Add Supremo.Infrastructure/Data/ApplicationDbContext.cs 1745277517697

---
+++ Supremo.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Supremo.Domain.Entities;
+
+namespace Supremo.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Appointment>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.CustomerName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Notes).HasMaxLength(500);
+            });
+        }
+    }
+}
