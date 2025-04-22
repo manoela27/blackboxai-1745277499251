0b76f68cd5c6ab517d88f2fd944dacdcc668cc88
Manoela Moreno Frattini <84809830+manoela27@users.noreply.github.com>
2025-04-21 21:20:33 -0300

    Add Supremo.Web/Models/ErrorViewModel.cs 1745281234789

---
+++ Supremo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Supremo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? Message { get; set; }
+    }
+}
